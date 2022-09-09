d6a3b58f2655ee1d48aeb29f5a6fbe8c64f0b7bd
iNeso1984 <vanessawithunweb@gmail.com>
2022-09-09 10:16:41 -0400

    upload express.js model_restAPI

---
+++ index.js
@@ -0,0 +1,40 @@
+// Import express
+import express from "express";
+
+//import the allRoutes function from our restRoute.js file
+import allRoutes from "./src/route/restRoute.js";
+
+//import mongoose
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+
+// Setup the port for the server
+const port = 4000;
+
+//set connection between the API and mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/studentDB", {
+  //avoid errors
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//parse requests and make it redable for our API
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//call the allRoute function and send app which initializes express
+allRoutes(app);
+
+// When a get request is made to / or the default page
+// display a message.
+app.get("/", (req, res) =>
+  res.send(`Your node and express server is running on port: ${port}`)
+);
+
+//print a message to the cli
+app.listen(port, () => {
+  console.log("restAPI is running on port: " + port);
+});
---
+++ src/route/restRoute.js
@@ -0,0 +1,35 @@
+//import required functions from restController
+import {
+  addStudent,
+  getStudents,
+  getStudentByID,
+  updateStudentByID,
+  deleteStudentByID,
+} from "../controller/restController.js";
+
+//Specific routes for different endpoints
+// such as get, post, delete and put
+const allRoutes = (app) => {
+  // we use student to get a list of all students or post a new student
+  app
+    .route("/student")
+
+    // call the getStudents function
+    .get(getStudents)
+
+    //call the addStudent function
+    .post(addStudent);
+
+  // we pass studentID to get, delete and update a specific student
+  app
+    .route("/student/:studentID")
+
+    .get(getStudentByID)
+
+    .delete(deleteStudentByID)
+
+    .put(updateStudentByID);
+};
+
+//export the allRoutes function so index.js can use it
+export default allRoutes;
